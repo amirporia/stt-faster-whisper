90d1d3b5c6a5dae4b962089a63877c70f1089f41
pouriya <amirporia99.1378@gmail.com>
2025-03-03 11:32:33 +0330

    check

--- backend/utils/methods.py
+++ backend/utils/methods.py
@@ -8,7 +8,7 @@ def remove_punctuation(text):
 
 
 def trim_last_incomplete_confirmed_sentence(confirmed_transciption, confirm_offset_time):
-    print("aaaaaaaaaaaaaaaaaa")
+
     if len(confirmed_transciption) > 0:
         idx = len(confirmed_transciption) - 1
         while idx != -1:
@@ -23,12 +23,12 @@ def trim_last_incomplete_confirmed_sentence(confirmed_transciption, confirm_offs
                     confirmed_transciption = confirmed_transciption[:idx + 1]
                 break
             idx -= 1
-    print("aaaaaaaaaaaaaaaaaaaa")
+
     return confirmed_transciption
 
 
 def confirmation_process(non_confirmed_transcription, tokenize_transcription, confirmed_transciption, confirm_offset_time):
-    print("bbbbbbbbbbbbbbbbbbbbb")
+    
     sliced_tokenize_transcription = [(a,b, remove_punctuation(" ".join(t.split()))) for a,b,t in tokenize_transcription]
 
     confirmed_transciption = trim_last_incomplete_confirmed_sentence(confirmed_transciption, confirm_offset_time)
@@ -37,7 +37,7 @@ def confirmation_process(non_confirmed_transcription, tokenize_transcription, co
         non_confirmed_transcription = sliced_tokenize_transcription
 
     elif len(non_confirmed_transcription) > 0:
-
+        print("11111111111111111111111111111")
         if confirm_offset_time == -1:
             if confirmed_transciption[-1][1] >= 5:
                 confirm_offset_time = confirmed_transciption[-1][1]
@@ -46,32 +46,31 @@ def confirmation_process(non_confirmed_transcription, tokenize_transcription, co
                 if ct_word[1] - confirm_offset_time >= 5:
                     confirm_offset_time = ct_word[1]
                     break
-
+        print("222222222222222222222222222")                    
         idx = 0
         for slice_idx in range(len(sliced_tokenize_transcription)):
             if sliced_tokenize_transcription[slice_idx][0] >= confirm_offset_time:
                 idx = slice_idx
                 break
-
+        print("33333333333333333333333333")        
         while idx < min(len(non_confirmed_transcription), len(sliced_tokenize_transcription)):
             if (non_confirmed_transcription[idx][2]).lower() == (sliced_tokenize_transcription[idx][2]).lower():
                 confirmed_transciption.append(non_confirmed_transcription[idx])
                 idx += 1
             else:
                 break
-
+        print("44444444444444444444444444")        
         if idx > len(non_confirmed_transcription):
             non_confirmed_transcription.extend(sliced_tokenize_transcription[idx:])
         else:
             non_confirmed_transcription[idx:] = sliced_tokenize_transcription[idx:]
-    print("bbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+
     return non_confirmed_transcription, confirmed_transciption, confirm_offset_time
 
 
 def sentence_trim_buffer(tokenize_transcription, non_confirmed_transcription, confirmed_transcription, confirm_offset_time, sample_rate=SAMPLE_RATE, bytes_per_sample=BYTES_PER_SAMPLE):
-    print("ccccccccccccccccccccccccccccccc")
+
     if len(confirmed_transcription) == 0:
-        print("ccccccccccccccccccccccccccccccc")
         return 0, non_confirmed_transcription, confirm_offset_time  # No confirmed sentences to remove
     
     # Find the last confirmed sentence ending with ., ?, or !
@@ -82,7 +81,6 @@ def sentence_trim_buffer(tokenize_transcription, non_confirmed_transcription, co
             break
 
     if not last_sentence:
-        print("ccccccccccccccccccccccccccccccc")
         return 0, non_confirmed_transcription, confirm_offset_time  # No complete sentence found, do not trim buffer
     
     confirmed_words = last_sentence.strip()
@@ -95,7 +93,6 @@ def sentence_trim_buffer(tokenize_transcription, non_confirmed_transcription, co
             end_time = end
 
     if end_time is None:
-        print("ccccccccccccccccccccccccccccccc")
         return 0, non_confirmed_transcription, confirm_offset_time  # No match found, do not trim buffer
     
     for idx in range(len(non_confirmed_transcription)):
@@ -103,19 +100,16 @@ def sentence_trim_buffer(tokenize_transcription, non_confirmed_transcription, co
             end_word_idx = idx
 
     if tokenize_transcription[-1][1] == end_time:
-        print("ccccccccccccccccccccccccccccccc")
         return -1, non_confirmed_transcription[end_word_idx + 1:], confirm_offset_time
     
     # Compute bytes to remove
     bytes_to_remove = int(end_time * sample_rate * bytes_per_sample)
 
     # Trim buffer 
-    print("ccccccccccccccccccccccccccccccc") 
     return bytes_to_remove, non_confirmed_transcription[end_word_idx + 1:], -1
 
 
 def threshold_trim_buffer(tokenize_transcription, non_confirmed_transcription, confirmed_transcription, buffer, sample_rate=SAMPLE_RATE, bytes_per_sample=BYTES_PER_SAMPLE):
-    print("dddddddddddddddddddddddddddddd")
     non_confirmed_transcription = []
     confirmed_transcription = trim_last_incomplete_confirmed_sentence(confirmed_transcription)
     end_time = 0
@@ -128,7 +122,6 @@ def threshold_trim_buffer(tokenize_transcription, non_confirmed_transcription, c
 
     if len(tokenize_transcription) > 0:
         non_confirmed_transcription.extend([(a,b,remove_punctuation(" ".join(t.split()))) for a,b,t in tokenize_transcription])
-        print("dddddddddddddddddddddddddddddd")
         return buffer[int(end_time * sample_rate * bytes_per_sample):], confirmed_transcription, non_confirmed_transcription, -1
-    print("dddddddddddddddddddddddddddddd")
+
     return bytearray(), confirmed_transcription, non_confirmed_transcription, -1
